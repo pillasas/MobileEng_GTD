51e6b9675435ad12e74c2648da9db243919cc471
pillasas <pillasas@students.zhaw.ch>
2021-07-08 16:22:55 +0200

    Tab AlleTasks

--- frontend/src/views/Tasks.vue
+++ frontend/src/views/Tasks.vue
@@ -5,21 +5,40 @@
         <ion-title>Alle Aufgaben</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Alle Aufgaben</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content>
+      <to-do-list v-bind:todoItems="todos"></to-do-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ToDoList from "@/components/ToDoList.vue";
+import { useTodos } from "@/composables/useTodos";
+import { defineComponent } from "@vue/runtime-core";
 
-export default  {
-  name: 'AllTasks',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default defineComponent({
+  name: "AllTasks",
+  components: {
+    IonContent,
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    ToDoList,
+  },
+  mounted() {
+    this.getTodos();
+  },
+  setup() {
+    const { todos, getTodos} = useTodos();
+    return { todos, getTodos};
+  },
+});
 </script>
